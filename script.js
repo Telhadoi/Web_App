edee61436c70da9837c034add2761707e80324ac
Telhadoi <ivan08mail.com@icloud.com>
2024-01-15 15:51:12 +0300

    scripts2
    scripts2

--- script.js
+++ script.js
@@ -9,5 +9,27 @@ buy.addEventListener('click' , () =>{
     document.getElementById('form').style.display = 'flex';
 })
 
+order.addEventListener('click', () => {
+    document.getElementById('error').innerHTML = '';
+    let name = document.getElementById('user_name').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+
+    if (name.length <2){
+        document.getElementById('error').innerText = 'Ошибка в имени';
+        return;
+    }
+
+    if (email.length < 5){
+        document.getElementById('error').innerText = 'Ошибка в email';
+        return;
+    }
+
+    if (phone.length , 5){
+        document.getElementById('error').innerText = 'Ошибка в номере';
+        return;
+    }
+
+})
 
 
